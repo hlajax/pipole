1b1e5869c8515ba0d7a00db9da316fe345b1e5b3
henry lepa malomalo <hlepa64@gmail.com>
2020-05-24 12:44:29 +0100

    webpack issue solved

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,4 @@
 const { environment } = require('@rails/webpacker')
 
-environment.plugins.prepend('Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery'
-  })
-)
 
 module.exports = environment
